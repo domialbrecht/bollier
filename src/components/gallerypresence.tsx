04cd165ca38ff18e87dd5b2ecac53faecd4a4483
Dominik Albrecht <dominik.albrecht@intersim.ch>
2022-07-26 00:30:12 +0200

    images and gallery scroll

--- src/components/gallerypresence.tsx
+++ src/components/gallerypresence.tsx
@@ -1,20 +1,40 @@
 import {motion, AnimatePresence} from 'framer-motion'
-import {useState} from 'react'
+import {useState, useRef, useEffect, useCallback} from 'react'
 import library from '../../images.json'
 
 function Gallery({images}: {images: any[]}) {
+  const [width, setWidth] = useState(0)
+  const carousel = useRef<HTMLDivElement>(null)
+  const scaleCarousel = useCallback(() => {
+    setWidth(carousel.current?.scrollWidth || 0)
+  }, [carousel])
+  useEffect(() => {
+    window.addEventListener('resize', scaleCarousel)
+    return () => {
+      window.removeEventListener('resize', scaleCarousel)
+    }
+  }, [carousel])
   return (
     <motion.div className="cursor-grab overflow-hidden">
-      <motion.div drag="x" className="flex gap-5">
+      <motion.div
+        drag="x"
+        ref={carousel}
+        dragConstraints={{
+          right: 0,
+          left: -(width - carousel.current?.clientWidth || 0),
+        }}
+        whileTap={{cursor: 'grabbing'}}
+        onLoad={scaleCarousel}
+        className="flex gap-5"
+      >
         <AnimatePresence exitBeforeEnter>
           {images.map((p, i) => (
             <motion.div
               className="flex flex-col gap-4"
               initial={{y: 200, opacity: 0}}
-              exit={{y: -200, opacity: 0}}
               animate={{y: 0, opacity: 1}}
               transition={{duration: 0.2, delay: i * 0.05}}
-              key={p.title}
+              key={i}
             >
               <img
                 alt="image"
